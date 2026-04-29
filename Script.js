0c81f492f5e7e55dcfa22441574186443cbada4c
Caio Kenji Sakamoto <sakamotocaiokenji@gmail.com>
2026-04-28 22:10:02 -0300

    Version 0.0.5

--- Script.js
+++ Script.js
@@ -4,5 +4,40 @@ function showPage(pageId) {
 }
 
 function submitApplication() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const resume = document.getElementById("resume").value.trim();
+
+    // ❌ Campos obrigatórios
+    if (name === "" || email === "") {
+        alert("You must fill all the obligatory fields.");
+        return;
+    }
+
+    // 👁️ Easter egg específico
+    if (
+        name.toLowerCase() === "dyle timesly" &&
+        email.toLowerCase() === "timesly.dyle@gardenview.com"
+    ) {
+        alert("YOUR TIME IS UP...");
+        revealTimesUp();
+        return;
+    }
+
+    // ✅ Resposta normal
     alert("Thank you for applying to work with us! Our team will carefully review your resumé and respond with news soon!");
+}
+
+function revealTimesUp() {
+    document.getElementById("dyleButton").style.display = "block"
+}
+
+function revealDyle() {
+    document.getElementById("dyleCard").style.display = "block";
+    document.getElementById("dandyCard").style.display = "none";
+}
+
+function hideDyle() {
+    document.getElementById("dyleCard").style.display = "none";
+    document.getElementById("dandyCard").style.display = "block";
 }
